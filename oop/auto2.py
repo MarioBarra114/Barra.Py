647b35658209cb2c28b4a9d43fb4a3b6c434fddc
MarioBarra114 <74979321+MarioBarra114@users.noreply.github.com>
2021-09-28 17:55:49 +0200

    Update auto2.py

--- oop/auto2.py
+++ oop/auto2.py
@@ -1,1 +1,47 @@
+'''
+Python3
+Programmazione ad oggetti
+'''
+
+class auto:
+
+    # Attributi di Classe
+    garanzia = 1
+    assicurazione = True
+    parcoAuto = 0
+
+    #Metodo costruttore
+    def __init__(self,proprietario, marca, modello, targa, colore, tipo_carburante):
+
+        # Attributi di Istanza
+        self.proprietario = proprietario
+        self.marca = marca
+        self.modello = modello
+        self.targa = targa
+        self.colore = colore
+        self.tipo_carburante = tipo_carburante
+        
+        auto.parcoAuto +=1
+
+    #Metodo di tipo Get
+    def scheda(self):
+        return f'\nScheda "{self.proprietario}"\n Marca: {self.marca}\n Modello: {self.modello}\n Targa: {self.targa}\n colore: {self.colore}\n assicurazione: {self.assicurazione}\n tipo carburante: {self.tipo_carburante}' 
+    
+giovanni = auto("giovanni","ford","fiesta","AX 123 XA", "rosso", "benzina verde")
+
+marco = auto("marco","fiat","Bravo","FB 104 HE", "verde", "gpl")
+
+print("Il tipo di variabile costruita è:")
+print(giovanni)
+print(marco)
+
+print("\nLa singola scheda è:")
+print (giovanni.scheda())
+print (marco.scheda())
+print("\nauto totali: ",auto.parcoAuto)
+
+print("\n\naltro metodo per visualizzare le informazioni (__dict__):")
+
+print(giovanni.__dict__)
+print(marco.__dict__)
 
