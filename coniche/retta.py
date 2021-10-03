199aca24de0632494997a903833e50fbe297a638
MarioBarra114 <74979321+MarioBarra114@users.noreply.github.com>
2021-10-03 22:21:30 +0200

    Update retta.py

--- coniche/retta.py
+++ coniche/retta.py
@@ -4,7 +4,7 @@ class retta:
     
 
     #Metodo costruttore
-    def __init__(self,a, b, c):
+    def __init__(self,a, b, c,x):
 
         # Attributi di Istanza
         self.a = a
@@ -13,10 +13,10 @@ class retta:
         
         
         
-        #self.m = -a/b
-       # self.q = -c/b
-        #self.x = x
-       # self.y = -b*x/a-c/a
+        self.m = -a/b
+        self.q = -c/b
+        self.x = x
+        self.y = -b*x/a-c/a
        
         
     #Metodo di tipo Get
@@ -24,13 +24,23 @@ class retta:
         return f'\nEquazione in forma implicita\n{self.a}x+{self.b}y+{self.c}=0' 
     
     
+    def esplicita(self):
+        return f'\nEquazione in forma esplicita\ny ={self.m}x+{self.q}' 
+    
+    
+     def tupla(self):
+        return f'\nla tupla è : A({self.a},{self.b})' 
+    
     
     
 retta1 = retta(2,8,-12)
 
-
-
 print(retta1.implicita())
+
+
+print(retta1.esplicita())
+
+print(retta1.tupla())
 
 
 print("\n\naltro metodo per visualizzare le informazioni (__dict__):")
